ac990d064027abef2cb54c3b6a1a2f7f1078b001
yuya <nerentabelen@mail.ru>
2023-12-16 19:18:05 +0300

    remove useless methods from form.py

--- courses/main/forms.py
+++ courses/main/forms.py
@@ -3,58 +3,6 @@ from django.core.validators import RegexValidator, MinValueValidator
 from .models import Student, Teacher
 
 
-# class StudentForm(forms.ModelForm):
-#     name_validator = RegexValidator(
-#         regex=r'^[a-zA-Zа-яА-Я\s]*$',
-#         message='Пожалуйста, введите только буквы.',
-#         code='invalid_name'
-#     )
-#
-#     full_name = forms.CharField(
-#         label='ФИО',
-#         validators=[name_validator],
-#         widget=forms.TextInput(attrs={'placeholder': 'Введите ФИО'})
-#     )
-#
-#     class Meta:
-#         model = Student
-#         fields = ['full_name', 'group']
-#         labels = {
-#             'group': 'Группа',
-#         }
-#
-#
-# class TeacherForm(forms.ModelForm):
-#     class Meta:
-#         model = Teacher
-#         fields = ['full_name', 'experience', 'course']
-#         labels = {
-#             'full_name': 'ФИО',
-#             'experience': 'Опыт',
-#             'course': 'Курс',
-#         }
-#
-#
-# class StudentEditForm(forms.ModelForm):
-#     class Meta:
-#         model = Student
-#         fields = ['full_name', 'group']
-#         labels = {
-#             'full_name': 'ФИО',
-#             'group': 'Группа',
-#         }
-#
-#
-# class TeacherEditForm(forms.ModelForm):
-#     class Meta:
-#         model = Teacher
-#         fields = ['full_name', 'experience', 'course']
-#         labels = {
-#             'full_name': 'ФИО',
-#             'experience': 'Опыт',
-#             'course': 'Курс',
-#         }
-
 class BasePersonForm(forms.ModelForm):
     name_validator = RegexValidator(
         regex=r'^[a-zA-Zа-яА-Я\s]*$',
